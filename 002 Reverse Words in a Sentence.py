be66e773c740bf2add405bccaf53809f6e12fec8
Thejashree <thejashreetheja7@gmail.com>
2026-07-22 21:33:38 +0530

    first commit

--- 002 Reverse Words in a Sentence.py
+++ 002 Reverse Words in a Sentence.py
@@ -4,6 +4,17 @@ words = sentence.split()
 
 reverse_words = words[::-1]
 
-result = " ".join(reverse_words)
+result = " ".join(reverse_words) 
+
+"""
+#result = " ".join(words)  output-Python love I
+#result = "-".join(words)  output-Python-love-I
+"""
+"""
+" " → Join with spaces
+"," → Join with commas
+"-" → Join with hyphens
+"" → Join without any separator
+"""                                                           
 
 print("Reversed Sentence:", result)
--- 003 Count Consonants in a Word.py
+++ 003 Count Consonants in a Word.py
@@ -6,5 +6,6 @@ count = 0
 for letter in word:
     if letter.isalpha() and letter not in vowels:
         count += 1
+#isalpha() = It checks whether letter is an alphabet (A-Z or a-z).
 
 print("Number of consonants:", count)
